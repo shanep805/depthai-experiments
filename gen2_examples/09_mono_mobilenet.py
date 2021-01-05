18e03d2d1fb6f50bc0e4afc2188cb0036ff66aac
SzabolcsGergely <szabi@luxonis.com>
2021-01-06 00:37:41 +0200

    Temp debug code

--- gen2_examples/09_mono_mobilenet.py
+++ gen2_examples/09_mono_mobilenet.py
@@ -8,27 +8,67 @@ import numpy as np
 # Start defining a pipeline
 pipeline = dai.Pipeline()
 
+cam_rgb = pipeline.createColorCamera()
+cam_rgb.setCamId(0)
+cam_rgb.setPreviewSize(300, 300)
+cam_rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+cam_rgb.setInterleaved(False)
+
+# Create output
+xout_rgb = pipeline.createXLinkOut()
+xout_rgb.setStreamName("rgb")
+cam_rgb.setPreviewSize(300, 300)
+cam_rgb.setInterleaved(False)
+cam_rgb.preview.link(xout_rgb.input)
+
+
+
+
 # Define a source - mono (grayscale) camera
 cam_left = pipeline.createMonoCamera()
 cam_left.setCamId(1)
 cam_left.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
 
+cam_right = pipeline.createMonoCamera()
+cam_right.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+cam_right.setCamId(2)
+
+
+depth = pipeline.createStereoDepth()
+depth.setConfidenceThreshold(200)
+depth.setOutputDepth(False)
+depth.setLeftRightCheck(False)
+depth.setExtendedDisparity(False)
+depth.setSubpixel(False)
+depth.setOutputRectified(False)
+depth.setMedianFilter(dai.StereoDepthProperties.MedianFilter.MEDIAN_OFF)
+# Note: the rectified streams are horizontally mirrored by default
+
+cam_left.out.link(depth.left)
+cam_right.out.link(depth.right)
+
+
+xout_depth = pipeline.createXLinkOut()
+xout_depth.setStreamName("depth")
+depth.disparity.link(xout_depth.input)
+
 # Define a neural network that will make predictions based on the source frames
 detection_nn = pipeline.createNeuralNetwork()
 detection_nn.setBlobPath(str((Path(__file__).parent / Path('models/mobilenet-ssd.blob')).resolve().absolute()))
+cam_rgb.preview.link(detection_nn.input)
 
 # Create a node to convert the grayscale frame into the nn-acceptable form
-manip = pipeline.createImageManip()
-manip.setResize(300, 300)
-# The NN model expects BGR input. By default ImageManip output type would be same as input (gray in this case)
-manip.setFrameType(dai.RawImgFrame.Type.BGR888p)
-cam_left.out.link(manip.inputImage)
-manip.out.link(detection_nn.input)
+# manip = pipeline.createImageManip()
+# manip.setResize(300, 300)
+# # The NN model expects BGR input. By default ImageManip output type would be same as input (gray in this case)
+# manip.setFrameType(dai.RawImgFrame.Type.BGR888p)
+# cam_rgb.preview.link(manip.inputImage)
+# manip.out.link(detection_nn.input)
 
-# Create outputs
-xout_manip = pipeline.createXLinkOut()
-xout_manip.setStreamName("left")
-manip.out.link(xout_manip.input)
+# # Create outputs
+# xout_manip = pipeline.createXLinkOut()
+# xout_manip.setStreamName("left")
+# manip.out.link(xout_manip.input)
 
 xout_nn = pipeline.createXLinkOut()
 xout_nn.setStreamName("nn")
@@ -41,6 +81,9 @@ device.startPipeline()
 # Output queues will be used to get the grayscale frames and nn data from the outputs defined above
 q_left = device.getOutputQueue("left")
 q_nn = device.getOutputQueue("nn")
+q_rgb_enc = device.getOutputQueue(name="h265", maxSize=8, overwrite=True)
+q_depth = device.getOutputQueue(name="depth", maxSize=8, overwrite=True)
+q_rgb = device.getOutputQueue(name="rgb", maxSize=4, overwrite=True)
 
 frame = None
 bboxes = []
@@ -50,11 +93,31 @@ bboxes = []
 def frame_norm(frame, bbox):
     return (np.array(bbox) * np.array([*frame.shape[:2], *frame.shape[:2]])[::-1]).astype(int)
 
+frame_depth = None
 
 while True:
     # instead of get (blocking) used tryGet (nonblocking) which will return the available data or None otherwise
-    in_left = q_left.tryGet()
+    in_left = None
+    # q_left.tryGet()
     in_nn = q_nn.tryGet()
+    in_rgb_enc = None
+    # q_rgb_enc.tryGet()
+    in_depth = q_depth.tryGet()
+
+    in_rgb = q_rgb.tryGet()  # blocking call, will wait until a new data has arrived
+
+    if in_rgb is not None:
+        # data is originally represented as a flat 1D array, it needs to be converted into HxWxC form
+        shape = (3, in_rgb.getHeight(), in_rgb.getWidth())
+        frame_rgb = in_rgb.getData().reshape(shape).transpose(1, 2, 0).astype(np.uint8)
+        frame_rgb = np.ascontiguousarray(frame_rgb)
+        cv2.imshow("rgb", frame_rgb)
+
+
+    if in_depth is not None:
+        frame_depth = in_depth.getData().reshape((in_depth.getHeight(), in_depth.getWidth())).astype(np.uint8)
+        frame_depth = np.ascontiguousarray(frame_depth)
+        frame_depth = cv2.applyColorMap(frame_depth, cv2.COLORMAP_JET)
 
     if in_left is not None:
         # if the grayscale frame data is available, transform the 1D data into a HxWxC frame
@@ -78,6 +141,9 @@ while True:
             bbox = frame_norm(frame, raw_bbox)
             cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
         cv2.imshow("left", frame)
+
+    if frame_depth is not None:
+        cv2.imshow("depth", frame_depth)
 
     if cv2.waitKey(1) == ord('q'):
         break
